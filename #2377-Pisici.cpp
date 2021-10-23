2a907f51da7666fde7e792f8a02946acf2846298
Ioana <87656331+Wizandroz@users.noreply.github.com>
2021-10-23 10:20:24 +0300

    Update #2377-Pisici.cpp

--- #2377-Pisici.cpp
+++ #2377-Pisici.cpp
@@ -3,8 +3,7 @@ using namespace std;
 int main()
 {
   long long n, m;
-    std::cout << "n= " << '\n';std::cin >> n;
-    std::cout << "m= " << '\n';std::cin >> m;
-    cout <<"timpul necesar ca pisica sa manance soarecii= " << n ;
+    cin >> n >> m;
+    cout << n;
     return 0;
 }
