118a2ced46be17c26d67d8422c85b61818cf8730
Ioana <87656331+Wizandroz@users.noreply.github.com>
2021-10-24 17:45:43 +0300

    Update #69-Iglindit.cpp

--- #69-Iglindit.cpp
+++ #69-Iglindit.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int main()
 {
   int n, ogl;
-  std::cout << "n=";std::cin >> n;
+  cin >> n;
   while (n != 0)
   {
     ogl = ogl * 10 + n % 10;
     n = n / 10;
   }
-  std::cout<<"oglindit=" << ogl <<'\n';
+  cout<<ogl;
   return 0;
 }
