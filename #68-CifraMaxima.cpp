ac1c79dc427e55f6eb555007deb51673711259ac
Ioana <87656331+Wizandroz@users.noreply.github.com>
2021-10-24 17:45:13 +0300

    Update #68-CifraMaxima.cpp

--- #68-CifraMaxima.cpp
+++ #68-CifraMaxima.cpp
@@ -3,13 +3,13 @@ using namespace std;
 int main()
 {
    int n, mx = 0;
-   std::cout << "n=";std::cin >> n;
+   cin >> n;
    while (n)
    {
       if (n % 10 > mx)
          mx = n % 10;
       n /= 10;
    }
-   std::cout <<"cifra maxima=" << mx <<'\n';
+   cout << mx;
    return 0;
 }
