ef83e07011dbd19d1f3cf4a1d7ec5123fc1b58b1
Ioana <87656331+Wizandroz@users.noreply.github.com>
2021-10-24 17:41:43 +0300

    Update #105-max2.cpp

--- #105-max2.cpp
+++ #105-max2.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int main()
 {
   int a, b, maxi;
-  std::cout << "a="; std::cin >> a;
-  std::cout << "b="; std::cin >> a;
+  cin >> a;
+  cin >> a;
   if (a>b) {
     maxi=a;
   }
   else
     maxi=b;
-  std::cout << "nr max=" << maxi << '\n';
+  cout << maxi;
 }
